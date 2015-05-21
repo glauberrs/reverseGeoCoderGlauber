31853b2505e6814f7c68f2129f141aa1d8953679
Aluno <aluno@SL04TTDESK05-2.local>
2015-05-21 08:59:05 -0300

    Initial Commit

---
+++ reverseGeoCoderGlauber/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reverseGeoCoderGlauber
+//
+//  Created by Aluno on 21/05/15.
+//  Copyright (c) 2015 Glauber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
